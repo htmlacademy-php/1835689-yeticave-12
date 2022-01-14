0c3535a855e4cf693f7037cb7063082ff3ff586b
Ольга Кашина <giperfrog@gmail.com>
2022-01-14 23:09:01 +0300

    Валидация формы

---
+++ add.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once('helpers.php');
+require_once('functions.php');
+require_once('connect.php');
+
+$sql = 'SELECT * FROM `categories`';
+
+$category_ids = [];
+
+if ($res = mysqli_query($link, $sql)) {
+    $categories = mysqli_fetch_all($res, MYSQLI_ASSOC);
+    $category_ids = array_column($categories, 'id');
+} else {
+    $content = include_template('error.php', ['error' => mysqli_error($link)]);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $required_fields = ['category', 'lot-name', 'message', 'lot-rate', 'lot-step', 'lot-date'];
+    $errors = [];
+
+    $rules = [
+        'category' => function($value) use ($category_ids) {
+            return validateCategory($value, $category_ids);
+        },
+        'lot-rate' => function($value) {
+            return validatePositive($value);
+        },
+        'lot-step' => function($value) {
+            return validatePositive($value);
+        },
+        'lot-date' => function($value) {
+            if(is_date_valid($value) === false) {
+                return "Введите дату в формате ГГГГ-ММ-ДД";
+            }
+        },
+        'lot-date' => function($value) {
+            return validateDate($value);
+        }
+    ];
+
+    $lot = filter_input_array(INPUT_POST, ['category' => FILTER_DEFAULT, 'lot-name' => FILTER_DEFAULT, 'message' => FILTER_DEFAULT, 'lot-rate' => FILTER_DEFAULT, 'lot-date' => FILTER_DEFAULT, 'lot-step' => FILTER_DEFAULT], true);
+
+    foreach ($lot as $key => $value) {
+        if (isset($rules[$key])) {
+            $rule = $rules[$key];
+            $errors[$key] = $rule($value);
+        }
+        if (in_array($key, $required_fields) && empty($value)) {
+            $errors[$key] = "Поле $key надо заполнить";
+        }
+    }
+
+    $errors = array_filter($errors);
+
+    if (!empty($_FILES['lot_img']['name'])) {
+        $tmp_name = $_FILES['lot_img']['tmp_name'];
+        $image = $_FILES['lot_img']['name'];
+        $filename = uniqid() . '.jpeg';
+
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $file_type = finfo_file($finfo, $tmp_name);
+
+        if ($file_type !== "image/jpeg" || "image/png") {
+            $errors['lot_img'] = "Загрузите файл в формате JPEG или PNG";
+        } else {
+            move_uploaded_file($tmp_name, 'uploads/' . $filename);
+            $lot['image'] = $filename;
+        }
+    } else {
+        $errors['lot_img'] = "Вы не загрузили файл";
+    }
+
+    if (count($errors)) {
+        $content = include_template('add-main.php', ['lot' => $lot, 'errors' => $errors, 'categories' => $categories]);
+    } else {
+        $sql = 'INSERT INTO `lots` (`dt_add`, `category_id`, `user_id`, `title`, `description`, `image`, `cost`, `dt_end`, `step`) VALUES (NOW(), ?, 1, ?, ?, ?, ?, ?, ?)';
+
+        $stmt = db_get_prepare_stmt($link, $sql, $lot);
+        $res = mysqli_stmt_execute($stmt);
+
+        if ($res) {
+            $lot_id = mysqli_insert_id($link);
+
+            header("Location: lot.php?id=" . $lot_id);
+        }
+    }
+} else {
+    $content = include_template('add-main.php', ['categories' => $categories]);
+}
+
+print(include_template('layout-inner.php', ['content' => $content, 'categories' => $categories]));
--- functions.php
+++ functions.php
@@ -1,15 +1,53 @@
 <?php
 
+//функция округляет число и разделяет разряды
+
 function formate_cost($form_cost) {
     $form_cost = ceil($form_cost);
     $form_cost = ($form_cost < 1000) ? $form_cost : number_format($form_cost, 0, '', ' ');
     return $form_cost . ' ' . '&#8381';
-};
+}
 
 function get_dt_range($date) {
     $cur_date = date_create('now');
     $date = date_create($date);
     $diff = date_diff($cur_date, $date);
     return $diff->d * 24 + $diff->h . ':' . $diff->i;
-};
+}
+
+//функция проверяет поле "Категория" на существующие в списке
+
+function validateCategory($id, $allowed_list) {
+    if (!in_array($id, $allowed_list)) {
+        return "Указана не существующая категория";
+    }
+    return null;
+}
+
+//функция проверяет на число больше нуля
+
+function validatePositive($value) {
+    if ($value <= 0) {
+        return "Введите положительное число";
+    }
+    return null;
+}
+
+//функция проверяет конечную дату
+
+function validateDate($date) {
+    $cur_date = date_create('now');
+    $date = date_create($date);
+    $diff = date_diff($cur_date, $date);
+    if ($diff->h < 24) {
+        return "Дата должна быть больше хотя бы на сутки";
+    }
+}
+
+//функция сохраняет введенное значение
+
+function getPostVal($name) {
+    return filter_input(INPUT_POST, $name);
+}
+
 ?>
