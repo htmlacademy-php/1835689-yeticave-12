9cff2e85f548a3c80c610151ff56a7eaeba92927
Ольга Кашина <giperfrog@gmail.com>
2022-05-03 16:54:39 +0300

    Добавила ввод ставки

--- enter.php
+++ enter.php
@@ -1,6 +1,5 @@
 <?php
 
-//session_start();
 require_once('helpers.php');
 require_once('functions.php');
 require_once('connect.php');
--- templates/lot-main.php
+++ templates/lot-main.php
@@ -10,22 +10,37 @@
                 <p class="lot-item__description"><?= htmlspecialchars($lot['description']); ?></p>
             </div>
             <div class="lot-item__right">
-            <?php if (isset($_SESSION['user'])) : ?>
-                <div class="lot-item__state">
-                    <? $res = get_dt_range($lot['dt_end']); ?>
-                    <div class="lot-item__timer timer <?= ($res < 1) ? 'timer--finishing' : ''; ?>"><?= $res; ?>
-                    </div>
-                    <div class="lot-item__cost-state">
-                        <div class="lot-item__rate">
-                            <span class="lot-item__amount">Текущая цена</span>
-                            <span class="lot-item__cost">10 999</span>
+                <?php if (isset($_SESSION['user'])) : ?>
+                    <div class="lot-item__state">
+                        <? $res = get_dt_range($lot['dt_end']); ?>
+                        <div class="lot-item__timer timer <?= ($res < 1) ? 'timer--finishing' : ''; ?>"><?= $res; ?>
                         </div>
-                        <div class="lot-item__min-cost">
-                            Мин. ставка <span><?= formate_cost($lot['cost']); ?></span>
+                        <div class="lot-item__cost-state">
+                            <div class="lot-item__rate">
+                                <span class="lot-item__amount">Текущая цена</span>
+                                <?php if ($cost_rate) : ?>
+                                <span class="lot-item__cost"><?= formate_cost($cost_rate); ?></span>
+                                <?php else : ?>
+                                <span class="lot-item__cost"><?= formate_cost($lot['cost']); ?></span>
+                                <?php endif; ?>
+                            </div>
+                            <div class="lot-item__min-cost">
+                                Мин. ставка <span><?= formate_cost($lot['cost']); ?></span>
+                            </div>
                         </div>
+                        <form class="lot-item__form" action="rate.php" method="post" autocomplete="off">
+                            <?php $classname = isset($errors['cost']) ? "form__item--invalid" : ""; ?>
+                            <p class="lot-item__form-item form__item <?= $classname ?>">
+                                <label for="cost">Ваша ставка</label>
+                                <input id="cost" type="text" name="cost" value="<?= getPostVal('cost'); ?>" placeholder="12 000">
+                                <?php if (isset($errors)) : ?>
+                                <span class="form__error">Введите наименование лота</span>
+                                <?php endif; ?>
+                            </p>
+                            <button type="submit" class="button">Сделать ставку</button>
+                        </form>
                     </div>
-                </div>
-            <?php endif; ?>
+                <?php endif; ?>
             </div>
         </div>
     </section>
